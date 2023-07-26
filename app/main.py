425f3493d111f57b7fbbc424dc5d039bfcd246fd
julia tong <juliatong2014thl@gmail.com>
2023-07-26 13:21:49 +0800

    enable live-reload

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import csv
 import pymysql.cursors
+# from models import Record
+# from connect import SessionLocal
+import uvicorn
 
 
 app = FastAPI()
@@ -8,15 +11,29 @@ app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    return {"message": "OK"}
+    return {"message": "OKabc"}
 
 
 # @app.post("/data")
-# def load_csv(request: Request):
+# async def load_csv(request: Request):
 #     with open('names.csv', newline='') as csvfile:
 #         reader = csv.DictReader(csvfile)
+#         data_to_insert = []
 #         for row in reader:
-#             print(row['first_name'], row['last_name'])
+#             print(row['UNIX'], row['SYMBOL'])
+#             data_to_insert.append(Record(UNIX=row['UNIX'],SYMBOL=row["SYMBOL"] ))
+
+#     db = SessionLocal
+#     try:
+#         db.add_all(data_to_insert)
+#         db.commit()
+#     except Exception as e:
+#         db.rollback()
+#         raise e
+#     finally:
+#         db.close()
+
+#     return {"message": "CSV data uploaded and inserted into the database."}
 
 
 

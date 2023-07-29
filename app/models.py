1554f452b354b8bc6cb8c5aa0e4819fbda810aa4
julia tong <juliatong2014thl@gmail.com>
2023-07-30 00:32:54 +0800

    specify (p,s) of decimal for ohlc_history table

--- app/models.py
+++ app/models.py
@@ -1,18 +1,20 @@
 from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
 import datetime as _dt
-import decimal
+from decimal import Decimal
 import connect
 from typing_extensions import Annotated
 from sqlalchemy.orm import registry
-from sqlalchemy import String
+from sqlalchemy import String, Numeric
 
 
 str_255 = Annotated[str, 255]
+num_65_9= Annotated[Decimal, 65]
 
 class Base(DeclarativeBase):
     registry = registry(
         type_annotation_map={
             str_255: String(255),
+            num_65_9: Numeric(65, 9),
         }
     )
 
@@ -20,8 +22,8 @@ class Record(Base):
     __tablename__ = 'ohlc_history'
     UNIX:Mapped[_dt.datetime] = mapped_column(nullable=False, default=_dt.datetime.utcnow, primary_key=True)
     SYMBOL:Mapped[str_255] = mapped_column(nullable=False, primary_key=True)
-    OPEN:Mapped[decimal.Decimal] = mapped_column(nullable=False)
-    CLOSE:Mapped[decimal.Decimal] = mapped_column(nullable=False)
-    HIGH:Mapped[decimal.Decimal] = mapped_column(nullable=False)
-    LOW:Mapped[decimal.Decimal]  = mapped_column(nullable=False)
+    OPEN:Mapped[num_65_9] = mapped_column(nullable=False)
+    CLOSE:Mapped[num_65_9] = mapped_column(nullable=False)
+    HIGH:Mapped[num_65_9] = mapped_column(nullable=False)
+    LOW:Mapped[num_65_9]  = mapped_column(nullable=False)
     
